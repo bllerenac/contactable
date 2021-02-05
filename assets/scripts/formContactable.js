a38b5c8902d378fec7bc5be16ce9a8e622107e05
Nick Correa <nickcorreagarcia@gmail.com>
2021-02-05 12:03:29 -0500

    render structure function formContactable

--- assets/scripts/formContactable.js
+++ assets/scripts/formContactable.js
@@ -3,7 +3,23 @@ export default function FormContactable(parentElement) {
     render: function () {
       const html = `
          <section>
+         <div>
          <h2>Contactable</h2>
+         <a class="js-logout" href="#logout"><i>Logout</i></a>
+         </div>
+         <div>
+         <p> Contacs(10)<p>
+         <li>Andrew Clark</li>
+         <li>Brian Vaughn</li>
+         <li>Dan Abramov/li>
+         <li>Dominic Gannaway</li>
+         <li>Luna Ruan</li>
+         <li>Rachel Nabors</li>
+         <li>Rick Hanlon</li>
+         <li>Yuzhi Zheng</li>
+         </div>
+         <button type="button">+</button>
+         
          </section>
       `;
       const parent = document.querySelector(parentElement);
