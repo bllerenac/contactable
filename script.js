9b76cecc1a7d96f25e23c2590f67f9188b442ba3
Marco Evair <mmacolluncoc@uni.pe>
2021-02-04 21:32:43 -0500

    add render for form newcontact and editnewcontact

---
+++ script.js
@@ -0,0 +1,9 @@
+import  NewContact from "./assets/scripts/newContact.js";
+console.log('hola2')
+function init() {
+    console.log('hola2')
+    const create = NewContact(".js-content")
+    create.render()
+}
+
+init()
