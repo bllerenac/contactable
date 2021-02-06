a8b7cc68588c71ae4b44e4f7586a6e0d410a1e5f
Nick Correa <nickcorreagarcia@gmail.com>
2021-02-05 22:14:21 -0500

    link icons and image

---
+++ assets/scripts/list.js
@@ -0,0 +1,7 @@
+// import { listContacts } from "./services/list_Contacts.js";
+// import STORE from "../store.js";
+
+// export default function getContacs() {
+//   const contacs = listContacts();
+//   console.log(contacs);
+// }
--- assets/scripts/renders/li_contacts.js
+++ assets/scripts/renders/li_contacts.js
@@ -1,23 +1,20 @@
 import STORE from "../store.js";
 export default function li_contacts() {
-  //   return {
-  //     parent: document.querySelector(parentElement),
-  //     render: function () {
-  //       const html = `
-  //       <div>
-  //         <img src="" alt="contact-image">
-  //         <li>Rick Hanlon</li>
-  //       </div>
-  //   `;
-  //       this.parent.innerHTML = html;
-  //     },
-  //   };
-  // }
-
-  return `
-<div>
-        <img src="" alt="contact-image">
-        <li>Rick Hanlon</li>
+  console.log(STORE.contacts);
+  const contacs_list = [];
+  STORE.contacts.forEach((element) => {
+    let html = `
+      <div class="contact">
+        <div class="default-image">
+          <img src="assets/icons/default-profile.png" alt="contact-image">
+        </div>
+        <li>${element.name}</li>
+        <div class="star-icon">
+          <i class="far fa-star"></i>
+        </div>
       </div>
 `;
+    contacs_list.push(html);
+  });
+  return contacs_list.join();
 }
